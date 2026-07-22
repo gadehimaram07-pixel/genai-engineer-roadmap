700ae41b33452c24d7b1abff6a7f7d94edcf0fca
Gade Himaram <gadehimaram07@gmail.com>
2026-07-22 21:33:28 +0530

    feat: Implemented Gemini AI Chatbot for conversation memory

--- week-03-APIs/gemini-chatbot/app.py
+++ week-03-APIs/gemini-chatbot/app.py
@@ -6,16 +6,16 @@ import traceback
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+chat = client.chats.create(
+    model = "gemini-flash-latest"
+)
 while True:
     prompt = input("USER: ")
     if prompt.lower()=="exit":
         print("Thank You. See you soon again!")
         break
     try:
-        response = client.models.generate_content(
-            model = "gemini-flash-latest",
-            contents = prompt
-        )
+        response = chat.send_message(prompt)
         print(response.text)
     except Exception as e:
         print(f"Error:{e}")
