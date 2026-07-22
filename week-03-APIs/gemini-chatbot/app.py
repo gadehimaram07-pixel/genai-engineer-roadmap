e1f4b3c0f4659a1e7aa4f92d5d3f6b8d115c4c7d
Gade Himaram <gadehimaram07@gmail.com>
2026-07-22 20:17:31 +0530

    feat: built Interactive Gemini Chatbot with error handling

--- week-03-APIs/gemini-chatbot/app.py
+++ week-03-APIs/gemini-chatbot/app.py
@@ -1,13 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import traceback
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model = "gemini-3-flash-preview",
-    contents = "What is Artificial Intelligence?"
-)
-
-print(response.text)
+while True:
+    prompt = input("USER: ")
+    if prompt.lower()=="exit":
+        print("Thank You. See you soon again!")
+        break
+    try:
+        response = client.models.generate_content(
+            model = "gemini-flash-latest",
+            contents = prompt
+        )
+        print(response.text)
+    except Exception as e:
+        print(f"Error:{e}")
